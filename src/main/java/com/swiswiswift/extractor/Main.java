ebc4f216230a4768f92d9608836323d9e2f9a1e1
小野 純平 <snorlax.chemist.and.jazz@gmail.com>
2021-11-01 16:04:20 +0900

    テストコードを追加

--- src/main/java/com/swiswiswift/extractor/Main.java
+++ src/main/java/com/swiswiswift/extractor/Main.java
@@ -12,27 +12,36 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) throws IOException {
 
+        // バイナリファイルを読み込む
         byte[] bytes = Files.readAllBytes(Paths.get("src/main/resources/sample1.nes"));
 
-        // 0-3: Constant $4E $45 $53 $1A ("NES" followed by MS-DOS end-of-file)
+        // NESファイルの0-3番目は $4E $45 $53 $1A と決まっている。("NES"+制御文字)
+        System.out.println("0: " + Integer.toHexString(bytes[0]));
+        System.out.println("1: " + Integer.toHexString(bytes[1]));
+        System.out.println("2: " + Integer.toHexString(bytes[2]));
+        System.out.println("3: " + Integer.toHexString(bytes[3]));
 
-        // 4: Size of PRG ROM in 16 KB units
+        // 4番目はプログラムROMのユニット数が入る。プログラムROMは1ユニット16KB。
         int programROMUnit = bytes[4];
         System.out.println("PRG ROM Unit: " + programROMUnit);
 
-        // 5: Size of CHR ROM in 8 KB units (Value 0 means the board uses CHR RAM)
+        // 5番目はキャラクターROMのユニット数が入る。キャラクターROMは1ユニット8KB。
         int characterROMUnit = bytes[5];
         System.out.println("CHR ROM Unit: " + characterROMUnit);
 
+        // ヘッダーは16Byte, プログラムROMは1ユニット16KB, キャラクターROMは1ユニット8KB。
         int NES_HEADER_SIZE = 16;       // 0x0010
-        int PROGRAM_ROM_SIZE = 16000;   // 0x4000;
-        int CHARACTER_ROM_SIZE = 8000;  // 0x2000;
+        int PROGRAM_ROM_UNIT_SIZE = 16000;   // 0x4000;
+        int CHARACTER_ROM_UNIT_SIZE = 8000;  // 0x2000;
 
-        int characterROMStart = NES_HEADER_SIZE + programROMUnit * PROGRAM_ROM_SIZE;
-        int characterROMEnd = characterROMStart + characterROMUnit * CHARACTER_ROM_SIZE;
+        // キャラクターROMのスタートと終わりのインデックスを取得。
+        int characterROMStart = NES_HEADER_SIZE + programROMUnit * PROGRAM_ROM_UNIT_SIZE;
+        int characterROMEnd = characterROMStart + characterROMUnit * CHARACTER_ROM_UNIT_SIZE;
 
+        // バイナリからキャラクターROMを取り出す。
         byte[] characterROM = Arrays.copyOfRange(bytes, characterROMStart, characterROMEnd);
 
+        // sprite用の配列を作成
         int[][][] sprites = new int[characterROM.length / 16][8][8];
 
         for (int i = 0; i < characterROM.length / 16; i++) {
@@ -42,15 +51,11 @@ public class Main {
             // sprite を初期化
             int[][] sprite = new int[8][8];
             for (int j = 0; j < 8; j++) {
-                int[] row = new int[8];
-                for (int k = 0; k < 8; k++) {
-                    row[k] = 0;
-                }
-                sprite[j] = row;
+                sprite[j] = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
             }
 
             for (int j = 0; j < 8; j++) {
-                String fixedValueString = byteToFixedWidthInteger(spriteBinary[j]);
+                String fixedValueString = byteToBinaryString(spriteBinary[j]);
                 for (int k = 0; k < 8; k++) {
                     if (fixedValueString.charAt(k) == '1') {
                         sprite[j][k] += 1;
@@ -59,7 +64,7 @@ public class Main {
             }
 
             for (int j = 8; j < 16; j++) {
-                String fixedValueString = byteToFixedWidthInteger(spriteBinary[j]);
+                String fixedValueString = byteToBinaryString(spriteBinary[j]);
                 for (int k = 0; k < 8; k++) {
                     if (fixedValueString.charAt(k) == '1') {
                         sprite[j - 8][k] += 2;
@@ -67,17 +72,52 @@ public class Main {
                 }
             }
 
-            // sprites
+            // sprite を追加
             sprites[i] = sprite;
         }
-
         exportImage(sprites);
     }
 
-    static String byteToFixedWidthInteger(byte value) {
+    /**
+     * byteを渡すと8文字のバイナリ文字列に変換する関数
+     *
+     * @param value ex. 8
+     * @return ex. 00001001
+     */
+    static String byteToBinaryString(byte value) {
         return String.format("%8s", Integer.toBinaryString(value & 0xFF)).replace(' ', '0');
     }
 
+    /**
+     * モック用のSpriteの配列を返す
+     * @return Stripe の配列
+     */
+    static int[][][] createMockStripes() {
+        int[][][] sprites = new int[500][8][8];
+
+        //
+        int[][] sprite = new int[8][8];
+        sprite[0] = new int[]{3, 1, 1, 3, 3, 1, 1, 3};
+        sprite[1] = new int[]{3, 1, 1, 1, 1, 1, 1, 3};
+        sprite[2] = new int[]{1, 2, 2, 1, 1, 2, 2, 1};
+        sprite[3] = new int[]{1, 2, 0, 2, 2, 0, 2, 1};
+        sprite[4] = new int[]{1, 2, 2, 2, 2, 2, 2, 1};
+        sprite[5] = new int[]{1, 2, 0, 0, 0, 0, 2, 1};
+        sprite[6] = new int[]{1, 2, 2, 2, 2, 2, 2, 1};
+        sprite[7] = new int[]{3, 1, 1, 1, 1, 1, 1, 3};
+
+        for (int i = 0; i < 500; i++) {
+            sprites[i] = sprite;
+        }
+        return sprites;
+    }
+
+    /**
+     * Sprite(int[][])の配列を渡すと画像にする関数。要素数は500である必要がある。
+     *
+     * @param sprites Sprite(int[][])の配列
+     * @throws IOException Exception
+     */
     static void exportImage(int[][][] sprites) throws IOException {
         // Image Size.
         int width = 800;
---
+++ src/test/java/com/swiswiswift/extractor/MainTest.java
@@ -0,0 +1,17 @@
+package com.swiswiswift.extractor;
+
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class MainTest {
+    @Test
+    public void testByteToBinaryString() throws Exception {
+        assertEquals("00000000", Main.byteToBinaryString((byte) 0));
+        assertEquals("00000010", Main.byteToBinaryString((byte) 2));
+        assertEquals("00000100", Main.byteToBinaryString((byte) 4));
+        assertEquals("00001000", Main.byteToBinaryString((byte) 8));
+        assertEquals("00001001", Main.byteToBinaryString((byte) 9));
+        assertEquals("11111111", Main.byteToBinaryString((byte) 255));
+    }
+}
